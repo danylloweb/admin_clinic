e750ec6787eaa2690b788606824f08654494a080
danylloweb <danyllophp@gmail.com>
2026-05-08 10:14:52 -0300

    ajuste de edicao de agendamento na tela de venda

--- resources/views/campaigns/create.blade.php
+++ resources/views/campaigns/create.blade.php
@@ -1,50 +1,52 @@
 @extends('layouts.header')
 @section('content')
-
         <div class="card text-white">
-            <div class="card-header">
-                <h4>Criar Campanha</h4>
-            </div>
             <div class="card-body">
                 <form id="formCreateCampaign">
                     @csrf
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label for="name" class="form-label">Nome da campanha</label>
-                            <input type="text" class="form-control" id="name" name="name" required>
-                        </div>
-                        <div class="col-md-6 mb-3">
-                            <label for="date" class="form-label">Data da campanha</label>
-                            <input type="date" class="form-control" id="date" name="date" required>
-                        </div>
-                    </div>
-
-                    <div class="row align-items-end">
-                        <div class="col-md-6 mb-3">
-                            <label for="image_file" class="form-label">Imagem da campanha</label>
-                            <input type="file" class="form-control" id="image_file" accept="image/*">
-                            <input type="hidden" id="url_image" name="url_image">
-                            <small class="text-muted">Selecione um arquivo de imagem.</small>
-                        </div>
-                        <div class="col-md-6 mb-2 mt-3 text-center">
-                            <img id="imagePreview" src="" alt="Preview da imagem" class="rounded img-fluid d-none" style="max-height: 300px;">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-12 mb-3">
-                            <label for="description" class="form-label">Descrição</label>
-                            <div class="border quill">
-                                <div class="quill-inner" id="description-editor" contenteditable="true"></div>
+                    <div class="row g-4">
+                        {{-- Coluna esquerda --}}
+                        <div class="col-md-6 d-flex flex-column gap-3">
+                            <div class="row g-2">
+                                <div class="col-6">
+                                    <label for="name" class="form-label">Nome da campanha</label>
+                                    <input type="text" class="form-control" id="name" name="name" required>
+                                </div>
+                                <div class="col-6">
+                                    <label for="date" class="form-label">Data da campanha</label>
+                                    <input type="date" class="form-control" id="date" name="date" required>
+                                </div>
                             </div>
-                            <input type="hidden" name="description" id="description">
+
+                            <div>
+                                <label for="image_file" class="form-label">Imagem da campanha</label>
+                                <input type="file" class="form-control" id="image_file" accept="image/*">
+                                <input type="hidden" id="url_image" name="url_image">
+                                <small class="text-muted">Selecione um arquivo de imagem.</small>
+                            </div>
+
+                            <div class="flex-grow-1 d-flex flex-column">
+                                <label for="description" class="form-label">Descrição</label>
+                                <div class="border quill flex-grow-1" style="min-height: 200px;">
+                                    <div class="quill-inner" id="description-editor" contenteditable="true"></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{-- Coluna direita: preview da imagem --}}
+                        <div class="col-md-6 d-flex align-items-start justify-content-center">
+                            <img id="imagePreview" src="" alt="Preview da imagem" class="rounded img-fluid d-none w-100" style="max-height: 650px; object-fit: contain;">
                         </div>
                     </div>
 
-                    <button type="submit" class="btn btn-success">
-                        <i class="fas fa-save me-1"></i> Criar campanha
-                    </button>
-                    <a href="{{ route('panel.campaigns.index') }}" class="btn btn-secondary ms-2">Cancelar</a>
+                    <input type="hidden" name="description" id="description">
+
+                    <div class="mt-4">
+                        <button type="submit" class="btn btn-success">
+                            <i class="fas fa-save me-1"></i> Criar campanha
+                        </button>
+                        <a href="{{ route('panel.campaigns.index') }}" class="btn btn-secondary ms-2">Cancelar</a>
+                    </div>
                 </form>
             </div>
         </div>
--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -1,56 +1,61 @@
 @extends('layouts.header')
 @section('content')
         <div class="card text-white">
-            <div class="card-header">
-                <h4>Editar Campanha -> {{ $campaign['id'] }}</h4>
-            </div>
             <div class="card-body">
                 <form id="formEditCampaign">
                     @csrf
                     <input type="hidden" name="id" value="{{ $campaign['id'] }}">
+                    @php
+                        $formattedDate = \Carbon\Carbon::parse($campaign['date'])->format('Y-m-d');
+                    @endphp
 
-                    <div class="row">
-                        <div class="col-md-6 mb-3">
-                            <label for="name" class="form-label">Nome da campanha</label>
-                            <input type="text" class="form-control" id="name" name="name" value="{{ $campaign['name'] }}" required>
-                        </div>
-                        @php
-                            $formattedDate = \Carbon\Carbon::parse($campaign['date'])->format('Y-m-d');
-                        @endphp
-                        <div class="col-md-6 mb-3">
-                            <label for="date" class="form-label">Data da campanha</label>
-                            <input type="date" id="date" class="form-control"  name="date" value="{{ $formattedDate }}" required>
-                        </div>
-                    </div>
+                    <div class="row g-4">
+                        {{-- Coluna esquerda --}}
+                        <div class="col-md-6 d-flex flex-column gap-3">
+                            <div class="row g-2">
+                                <div class="col-6">
+                                    <label for="name" class="form-label">Nome da campanha</label>
+                                    <input type="text" class="form-control" id="name" name="name" value="{{ $campaign['name'] }}" required>
+                                </div>
+                                <div class="col-6">
+                                    <label for="date" class="form-label">Data da campanha</label>
+                                    <input type="date" id="date" class="form-control" name="date" value="{{ $formattedDate }}" required>
+                                </div>
+                            </div>
 
-                    <div class="row align-items-end">
-                        <div class="col-md-6 mb-lg-20">
-                            <label for="image_file" class="form-label">Imagem da campanha</label>
-                            <input type="file" class="form-control" id="image_file" accept="image/*">
-                            <input type="hidden" id="url_image" name="url_image" value="{{ $campaign['url_image'] }}">
-                            <small class="text-muted">Selecione um arquivo para substituir a imagem atual.</small>
-                        </div>
-                        <div class="col-md-6 mb-2 mt-3 text-center">
-                            <img id="imagePreview" src="{{ $campaign['url_image'] }}" alt="Imagem atual" class="rounded img-fluid" style="max-height: 300px;">
-                        </div>
-                    </div>
+                            <div>
+                                <label for="image_file" class="form-label">Imagem da campanha</label>
+                                <input type="file" class="form-control" id="image_file" accept="image/*">
+                                <input type="hidden" id="url_image" name="url_image" value="{{ $campaign['url_image'] }}">
+                                <small class="text-muted">Selecione um arquivo para substituir a imagem atual.</small>
+                            </div>
 
-                    <div class="row">
-                        <div class="col-12 mb-3">
-                            <label for="description" class="form-label">Descrição</label>
-                            <div class="border quill">
-                                <div class="quill-inner" id="description-editor">{{ $campaign['description'] }}</div>
+                            <div class="flex-grow-1 d-flex flex-column">
+                                <label for="description" class="form-label">Descrição</label>
+                                <div class="border quill flex-grow-1" style="min-height: 200px;">
+                                    <div class="quill-inner" id="description-editor">{{ $campaign['description'] }}</div>
+                                </div>
                             </div>
                         </div>
+
+                        {{-- Coluna direita: preview da imagem --}}
+                        <div class="col-md-6 d-flex align-items-start justify-content-center">
+                            <img id="imagePreview" src="{{ $campaign['url_image'] }}" alt="Imagem atual"
+                                 class="rounded img-fluid w-100" style="max-height: 650px; object-fit: contain;">
+                        </div>
                     </div>
+
                     <input type="hidden" name="description" id="description" value="{{ $campaign['description'] }}">
-                    <button type="submit" class="btn btn-success">
-                        <i class="fas fa-save me-1"></i> Salvar alterações
-                    </button>
-                    <a href="{{ route('panel.campaign.send', ['id' => $campaign['id']]) }}" class="btn btn-primary ms-2">
-                        Enviar campanha
-                    </a>
-                    <a href="{{ route('panel.campaigns.index') }}" class="btn btn-secondary ms-2">Voltar</a>
+
+                    <div class="mt-4">
+                        <button type="submit" class="btn btn-success">
+                            <i class="fas fa-save me-1"></i> Salvar alterações
+                        </button>
+                        <a href="{{ route('panel.campaign.send', ['id' => $campaign['id']]) }}" class="btn btn-primary ms-2">
+                            Enviar campanha
+                        </a>
+                        <a href="{{ route('panel.campaigns.index') }}" class="btn btn-secondary ms-2">Voltar</a>
+                    </div>
                 </form>
             </div>
         </div>
